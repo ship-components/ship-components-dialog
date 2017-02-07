ef39bcc7c132f9992efcce1ec63309cf9d58763e
Chris Orescan <chris_orescan@playstation.sony.com>
2017-02-07 13:18:41 -0800

    export const (not func)

--- src/Dialog.js
+++ src/Dialog.js
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import Modal from './Modal';
 import { Button, ButtonGroup } from 'ship-components-buttons';
 import css from './Dialog.css';
-import {getScrollingDialogClassname} from "./index";
+import {DROPDOWN_PARENT_CLASS} from "./index";
 
 export default class Dialog extends Component {
 
@@ -38,7 +38,7 @@ export default class Dialog extends Component {
         {this.props.name ?
           <h1 className={css.title}>{this.props.name}</h1>
         : null}
-        <div className={classNames(css.body, getScrollingDialogClassname())}>
+        <div className={classNames(css.body, DROPDOWN_PARENT_CLASS)}>
           {this.props.message || this.props.children}
         </div>
         <ButtonGroup
